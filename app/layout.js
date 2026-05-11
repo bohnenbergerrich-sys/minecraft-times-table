d34ca08cf7ef7ff268bd18d4f1799818dedf2b4d
bohnenbergerrich-sys <bohnenberger.rich@gmail.com>
2026-05-11 17:15:37 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "MathCraft v0.10.1",
+  title: "MathCraft v0.10.2",
   description: "Dungeon loop with boss reveal and monster gallery",
 };
 
